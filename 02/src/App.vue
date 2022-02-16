9c67076cba99d70ca2ee7fbf8ea0b718f7e3ba2c
João Henrique <joaohenriquetdj@gmail.com>
2022-02-16 13:32:38 -0300

    life cycle hooks

--- 02/src/App.vue
+++ 02/src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <FirstComponent title="teste" />
+  <LifeCycle />
 </template>
 
 <script>
-import FirstComponent from "./components/FirstComponent.vue";
+import FirstComponent from "./components/FirstComponent";
+import LifeCycle from "./components/LifeCycle";
 export default {
   name: "App",
   components: {
     FirstComponent,
+    LifeCycle,
   },
 };
 </script>
---
+++ 02/src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Meu nome é {{ name }}</h1>
+</template>
+
+<script>
+export default {
+  name: "LyfeCycle",
+  data() {
+    return {
+      name: "Nome",
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.name = "João";
+    }, 1000);
+  },
+  mounted() {
+    setTimeout(() => {
+      this.name = "Pedro";
+    }, 2000);
+  },
+};
+</script>
