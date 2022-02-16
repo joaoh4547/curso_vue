949fe4be2f0879de4aeb9aaf9813821a848bcd27
João Henrique <joaohenriquetdj@gmail.com>
2022-02-16 13:10:24 -0300

    create vue component

---
+++ 02/src/components/FirstComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1></h1>
+    <p>Meu Nome é {{ name }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FirstComponent",
+  data() {
+    return {
+      name: "João",
+    };
+  },
+};
+</script>
